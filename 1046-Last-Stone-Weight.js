ff2a624928d3c4a097845dc0188ec14180a53075
asuthamm <asuthamm@gmail.com>
2020-08-26 23:40:27 -0400

    update 1046-Last-Stone-Weight

--- 1046-Last-Stone-Weight.js
+++ 1046-Last-Stone-Weight.js
@@ -20,3 +20,12 @@ lastStoneWeight([2,7,4,1,8,1]); // 1
 
 
 // Math.max(...[1,3,5,7])
+// another option
+const lastStoneWeight = function(stones) {
+  stones.sort((a, b) => a - b)
+  while (stones.length > 1) {
+    const num = Math.abs(stones.pop() - stones.pop())
+    stones.splice(stones.findIndex(item => item >= num), 0, num)
+  }
+  return stones[0] 
+};
