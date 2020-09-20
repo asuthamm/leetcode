4e4a3aa0ffd36d9169f76c621a4243d2ce822cf7
Alex Suthammanont <43193919+asuthamm@users.noreply.github.com>
2020-09-20 16:49:35 -0400

    change the return of idx instead of value

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -1,19 +1,18 @@
 var twoSum = function(nums, target) {
 
   let lookup = [];
-    
+
   for (let i in nums) {
     let curr = nums[i];
     let counterpart = target - curr;
-    
+
     if (lookup.indexOf(counterpart) !== -1) {
-      return [curr, counterpart];
+      // counterpart found, return idx of the two sum
+      return [nums.indexOf(curr), nums.indexOf(counterpart)];
     }
     lookup.push(curr);    
-    //console.log(`At the end of idx ${i}, curr=${curr}`);
-    //console.log('lookup=', lookup);  
-    //console.log(' ');      
   }    
+  return 0;
 };
 
 twoSum([1,2,3,4],5)
