7355853ba417aa152c1ad481a9e5d395fe45feff
Alex Suthammanont <asuthamm@gmail.com>
2020-12-14 07:11:59 -0500

    update median-of-arrays

--- byte-by-byte/median-of-arrays.js
+++ byte-by-byte/median-of-arrays.js
@@ -4,11 +4,12 @@ const arr1 = [1,3,5];
 const arr2 = [2,4,6];
 
 const median = (arr1, arr2) => {
-  const arr =  arr1.concat(arr2);
-  const mid = Math.floor(arr.length / 2);
-  const sorted = [...arr].sort((a, b) => a - b);
-//   console.log(sorted, mid);
+  const merged = arr1.concat(arr2);
+  const sorted = [...merged].sort((a, b) => a - b);
+  const mid = Math.floor(merged.length / 2);
+  // console.log(sorted, mid);
 
-  return arr.length % 2 !== 0 ? sorted[mid] : (sorted[mid - 1] + sorted[mid]) / 2;
+  return merged.length % 2 !== 0 ? sorted[mid] : 
+         (sorted[mid - 1] + sorted[mid]) / 2;
 }
 median(arr1, arr2);
